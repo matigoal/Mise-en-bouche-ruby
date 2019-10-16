9e645a3e052314a92d5212086b6ecd75f15b5667
matigoal <55578343+matigoal@users.noreply.github.com>
2019-10-16 02:24:52 +0200

    Delete Group_Owners.rb

--- testdome/ Propriétaires/Group_Owners.rb
+++
@@ -1,6 +0,0 @@
-def group_by_owners(files)
-  better_hash = Hash.new { |hash, key| hash[key] = [] }
-  files.each_with_object(better_hash) {|(k, v), hash| hash[v] << k}
-end
-
-group_by_owners({'Input.txt' => 'Randy', 'Code.py' => 'Stan', 'Output.txt' => 'Randy'})
